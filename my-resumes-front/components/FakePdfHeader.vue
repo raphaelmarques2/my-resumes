b760faaf56c1c97adb121230263307240f003136
Raphael Marques <jose.raphael.marques@gmail.com>
2023-11-02 16:16:48 -0300

    hide profile items when they are empty

--- my-resumes-front/components/FakePdfHeader.vue
+++ my-resumes-front/components/FakePdfHeader.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="text-xs space-y-1 mt-2">
     <div>
-      <p class="text-base font-bold text-center">{{ $props.profile.name }}</p>
-      <p class="font-bold text-center">{{ $props.resume.title }}</p>
+      <p class="text-base font-bold text-center">{{ profile.name }}</p>
+      <p class="font-bold text-center">{{ resume.title }}</p>
     </div>
     <div>
-      <p class="">{{ $props.profile.address }}</p>
-      <p class="">E-mail: {{ $props.profile.email }}</p>
-      <p class="">LinkedIn: {{ $props.profile.linkedin }}</p>
+      <p v-if="profile.address">{{ profile.address }}</p>
+      <p v-if="profile.email">E-mail: {{ profile.email }}</p>
+      <p v-if="profile.linkedin">LinkedIn: {{ profile.linkedin }}</p>
     </div>
     <div class="border-b border-black w-full"></div>
     <p class="font-bold">Profile</p>
-    <p class="text-justify">{{ $props.resume.description }}</p>
+    <p class="text-justify">{{ resume.description }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ProfileDto, ResumeDto } from "~/services/backend/generated";
 
-const props = defineProps<{
+defineProps<{
   profile: ProfileDto;
   resume: ResumeDto;
 }>();
