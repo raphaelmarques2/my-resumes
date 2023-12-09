2be1ef83f719a2051e0cbf2334d536da9497323b
Raphael Marques <jose.raphael.marques@gmail.com>
2023-12-09 15:12:52 -0300

    astro site with resumes and profile

--- my-resumes-astro/astro.config.mjs
+++ my-resumes-astro/astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  redirects: {
+    "/resumes/[id]": "/resumes/[id]/resume"
+  }
 });
